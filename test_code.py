d0c611965b9b0dcc1f543c0434c3ac95a2e3bc89
Anantha Krishna Rajpurohit <anantha.ani@gmail.com>
2021-06-16 23:02:15 +0530

    Create test_code.py

---
+++ test_code.py
@@ -0,0 +1,9 @@
+# this is a code to test git clone
+import os
+import time
+from datetime import datetime
+
+# get current date
+datetime_object = datetime.now()
+print("You have successfully cloned the test repository on", datetime_object.day, "of", datetime_object.strftime("%B"), datetime_object.year, "with exact timestamp of", datetime_object)
+
