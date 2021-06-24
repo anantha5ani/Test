af44438cc41052a1a8d0b3b969e9d1860d7a96b3
Anantha <anantha.ani@gmail.com>
2021-06-24 19:59:51 +0530

    updated with notebook

--- test_code.py
+++ test_code.py
@@ -1,9 +1,6 @@
-# this is a code to test git clone
-import os
-import time
+# this is a code to test script run
 from datetime import datetime
 
 # get current date
 datetime_object = datetime.now()
-print("You have successfully cloned the test repository on", datetime_object.day, "of", datetime_object.strftime("%B"), datetime_object.year, "with exact timestamp of", datetime_object)
-
+print("You have successfully tested the script on", datetime_object.day, "of", datetime_object.strftime("%B"), datetime_object.year, "with exact timestamp of", datetime_object)
